d1747912fa1f67f049c459004330655d5e001676
Luka Vandervelden <lukc@upyum.com>
2019-12-09 23:31:45 +0100

    Added client index.html and project.zsh entries.

--- project.zsh
+++ project.zsh
@@ -7,4 +7,12 @@ type[authd]=crystal
 sources[authd]=src/main.cr
 depends[authd]="$(ls src/*.cr | grep -v '/main.cr$' | tr '\n' ' ')"
 
+targets+=(client/main.js)
+type[client/main.js]=livescript
+sources[client/main.js]=client/index.ls
+depends[client/main.js]="$(ls client/*.ls | grep -v '/index.ls$' | tr '\n' ' ')"
+
+targets+=(client/style.css)
+type[client/style.css]=sass
+sources[client/style.css]=client/style.sass
 
